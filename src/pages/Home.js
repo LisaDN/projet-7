9ab60ceedbf22859465f3bccb3ee480a7e63abb3
Lisa DN <dorvillelisa17@gmail.com>
2022-12-12 11:29:21 +0100

    initialisation projet kasa

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import Header from "../components/Header";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+// const Home = () => {
+//   return (
+//     <div>
+//      <Header />
+//     </div>
+//   );
+// };
+// export default Home;
