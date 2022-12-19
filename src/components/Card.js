51c09565281cc206d47334cd1300a198ee6cc2b0
Lisa DN <dorvillelisa17@gmail.com>
2022-12-19 13:54:02 +0100

    mise en page card page accueil

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 export default function Card({ accomodation }) {
   return (
     <div className="cardHome">
-      <img src={accomodation.cover} alt="logement" />
+      <img src={accomodation.cover} alt={accomodation.tags} />
       <h2>{accomodation.title}</h2>
     </div>
   );
